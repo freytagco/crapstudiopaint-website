32c237f4874bd3e23f21b4fd151ad024707981c9
freytagco <freytagco@gmail.com>
2025-08-07 12:29:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,73 @@
 document.addEventListener("DOMContentLoaded", () => {    
     const button1 = document.getElementById("button1");
-      document.body.appendChild(button1);
-      
-      const button2 = document.getElementById("button2");
-      document.body.appendChild(button2);
+    const button2 = document.getElementById("button2");
+    const button3 = document.getElementById("button3");
+    const button4 = document.getElementById("button4");
 
-      const button3 = document.getElementById("button3");
-      document.body.appendChild(button3);
+    const comingsoonButton = document.querySelector(".comingsoonButton");
 
-      const button4 = document.getElementById("button4");
-      document.body.appendChild(button4);
+    const emailcontactButton = document.querySelector(".emailcontactButton");
       
-      const overlay1 = document.getElementById("overlay1");
-      const overlay2 = document.getElementById("overlay2");
-      const overlay3 = document.getElementById("overlay3");
-      const overlay4 = document.getElementById("overlay4");
+    const overlay1 = document.getElementById("overlay1");
+    const overlay2 = document.getElementById("overlay2");
+    const overlay3 = document.getElementById("overlay3");
+    const overlay4 = document.getElementById("overlay4");
+    const contactoverlay = document.getElementById("contactoverlay");
+
+    const closeButton1 = document.getElementById("closeButton1");
+    const closeButton2 = document.getElementById("closeButton2");
+    const closeButton3 = document.getElementById("closeButton3");
+    const closeButton4 = document.getElementById("closeButton4");
+    const closeButtonContact = document.getElementById("closeButtonContact");
+    const overlaybackground = document.getElementById("overlaybackground");
       
-      const closeButton1 = document.getElementById("closeButton1");
-      const closeButton2 = document.getElementById("closeButton2");
-      const closeButton3 = document.getElementById("closeButton3");
-      const closeButton4 = document.getElementById("closeButton4");
+    overlay1.style.display = 'none';
+    overlay2.style.display = 'none';
+    overlay3.style.display = 'none';
+    overlay4.style.display = 'none';
+    contactoverlay.style.display = 'none';
+    overlaybackground.style.display = 'none';
       
-      overlay1.style.display = 'none';
-      overlay2.style.display = 'none';
-      overlay3.style.display = 'none';
-      overlay4.style.display = 'none';
-      overlaybackground.style.display = 'none';
-      
+    if (button1) {
       button1.onclick = () => {
         overlay1.style.display = 'block';
         overlaybackground.style.display = 'block';
       };
+    }
 
+    if (button2) { 
       button2.onclick = () => {
         overlay2.style.display = 'block';
         overlaybackground.style.display = 'block';
       };
+    }
 
+    if (button3) {
       button3.onclick = () => {
         overlay3.style.display = 'block';
         overlaybackground.style.display = 'block';
       };
+    }
 
+    if (button4) {
       button4.onclick = () => {
         overlay4.style.display = 'block';
         overlaybackground.style.display = 'block';
       };
+    }
+
+    if (comingsoonButton) {
+      comingsoonButton.onclick = () => {
+        contactoverlay.style.display = 'block';
+        overlaybackground.style.display = 'block';
+      };
+    }
+
+    if (emailcontactButton) {
+      emailcontactButton.onclick = () => {
+        window.open("mailto:freytagco@gmail.com", "_blank");
+      }
+    }
       
       closeButton1.onclick = () => {
         overlay1.style.display = 'none';
@@ -64,6 +86,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
       closeButton4.onclick = () => {
         overlay4.style.display = 'none';
+        overlaybackground.style.display = 'none';
+      };
+
+      closeButtonContact.onclick = () => {
+        contactoverlay.style.display = 'none';
         overlaybackground.style.display = 'none';
       };
     });
